af6510049f30bda238aff26fc086ec3715626edb
Arvind <arvindmepani@gmail.com>
2018-08-07 16:51:53 +0530

    Initial Commit after copied repo from saineshwar account.

---
+++ Issue List.md
@@ -0,0 +1,8 @@
+## This file contains known issue or existing issue in WebTimeSheetManagement created by @Saineshwar as of 07-Aug-2018.
+
+### I am updating this document to track issue and status
+
+ - [ ] ErrorView/Error Request goes to infinite loops as that route is not available. Same for any 404 error it redirect in infinite loops
+ - [ ] Login or redirect after session timeout is not handled correctly, currently it will redirect to Error/Error instead of login page.
+ - [ ] Broken /AllExpense/ApprovedExpense page 
+ - [ ] packages/bin/obj folder included in git version control which is not required to add in source code.
---
+++ WebTimeSheetManagement/Global.asax.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using WebTimeSheetManagement.Filters;
+
+namespace WebTimeSheetManagement
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_BeginRequest()
+        {
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetExpires(DateTime.UtcNow.AddHours(-1));
+            Response.Cache.SetNoStore();
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalFilters.Filters.Add(new UserAuditFilter());
+            SqlDependency.Start(ConfigurationManager.ConnectionStrings["TimesheetDBEntities"].ToString());
+        }
+
+
+        void Application_Error(object sender, EventArgs e)
+        {
+
+            Exception ex = Server.GetLastError();
+            if (ex == null || ex.Message.StartsWith("File"))
+            {
+                return;
+            }
+            try
+            {
+                Server.ClearError();
+                // Below line execute in infinite loops as below path is not found so updated code
+                //Response.Redirect("~/Errorview/Error");
+
+                // Updated code for redirect to Error page in general 
+                //need to create separate page for Well known error. e.g. 404 Error
+                Response.Redirect("~/Error/Error");
+
+
+            }
+            finally
+            {
+                ex = null;
+            }
+
+        }
+    }
+}
